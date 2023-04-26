dd328526808b9b1f6dd327c1c9a53c9b52c6b3f8
igormaxlima <igormaxnunes@gmail.com>
2023-04-26 15:45:14 -0300

    Implementando requisição do servidor local http
    para implementar o backend na aplicação foi usado a biblioteca axios com
    os metodos GET e POST do backend, desse modo, foi implementado dois
    metodos de requisição com o axios: o método then e catch, e o método
    async await.
    
    necessario instalar a biblioteca axios com o comando "npm install axios"

--- sprintVP/App.js
+++ sprintVP/App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [mensagem, setMensagem] = useState('')
+
+  useEffect(() => {
+    // fazendo promisse utilizando then e catch
+    // axios.post('http://172.20.10.7:3000/ola', { nome: "Vamos Parcelar!" })
+    //   .then(response => {
+    //     setMensagem(response.data);
+    //   })
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
+
+    // fazendo requisicao utilizando metodo async, await
+      async function fetchMensagem() {
+        try {
+          const response = await axios.post('http://172.20.10.7:3000/ola', { nome: "Vamos Parcelar!" })
+          setMensagem(response.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      fetchMensagem();
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
+      <Text>{ mensagem }</Text>
     </View>
   );
 }
