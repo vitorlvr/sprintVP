abf7699b9f418998717d9e49a088d4f8a579bf47
igormaxlima <igormaxnunes@gmail.com>
2023-04-28 17:58:23 -0300

    adicionado botao que recebe nome e imprime nome do usuario

--- sprintVP/App.js
+++ sprintVP/App.js
@@ -1,37 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [mensagem, setMensagem] = useState('')
+  const [mensagem, setMensagem] = useState('');
+  const [name, setName] = useState('');
 
-  useEffect(() => {
-    // fazendo promisse utilizando then e catch
-    // axios.post('http://172.20.10.7:3000/ola', { nome: "Vamos Parcelar!" })
-    //   .then(response => {
-    //     setMensagem(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.error(error);
-    //   });
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://172.20.10.7:3000/ola', { nome: name })
+      setMensagem(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    // fazendo requisicao utilizando metodo async, await
-      async function fetchMensagem() {
-        try {
-          const response = await axios.post('http://172.20.10.7:3000/ola', { nome: "Vamos Parcelar!" })
-          setMensagem(response.data);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-      fetchMensagem();
-  }, []);
-  
   return (
     <View style={styles.container}>
-      <Text>{ mensagem }</Text>
+      <Text style={styles.textoPrincipal}>{mensagem}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome:"
+        value={name}
+        onSubmitEditing={handleSubmit}
+        onChangeText={(text) => setName(text)}
+      />
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.textoBotao}>Enviar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -43,4 +39,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textoPrincipal: {
+    fontSize: 30,
+  },
+  input: {
+    marginTop: 20,
+    width: 150,
+    height: 35,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 6,
+    padding: 8,
+  },
+  button: {
+    backgroundColor: "#2A9F85",
+    width: 150,
+    height: 35,
+    margin: 5,
+    borderWidth: 1,
+    borderRadius: 6,
+    padding: 5,
+  },
+  textoBotao: {
+    textAlign: 'center',
+    color: "#FFFFFF",
+    fontSize: 20,
+    lineHeight: 26,
+    fontWeight: "bold",
+  }
 });
