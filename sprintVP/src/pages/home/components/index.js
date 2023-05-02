09a03b1e4b79fd68033e8255a531ee2f084ae74b
Vitor Souza <87672709+vitorlvr@users.noreply.github.com>
2023-05-02 16:17:48 -0300

    Organizando arquivos e pastas

---
+++ sprintVP/src/pages/home/components/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5619b4',
+}});
