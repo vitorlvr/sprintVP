17b2e8bf36fee3b811afba8cce2e853f29d25b2b
igormaxlima <igormaxnunes@gmail.com>
2023-05-19 14:43:41 -0300

    Removendo constantes do servidor cartoes.js

--- backend/cartoes.js
+++ backend/cartoes.js
@@ -5,22 +5,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const cards = [
-  {
-    id: 1,
-    name: 'Visa',
-    number: '1234567812345678',
-    expirationDate: '10/25',
-    cvv: '123'
-  },
-  {
-    id: 2,
-    name: 'MasterCard',
-    number: '9876543298765432',
-    expirationDate: '12/27',
-    cvv: '456'
-  }
-];
+cards = []
+
 
 app.get('/cards', (req, res) => {
   res.json(cards);
