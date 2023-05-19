f59c5bef64e996ff2d87997cb503ce797dd7487d
Vitor Souza <87672709+vitorlvr@users.noreply.github.com>
2023-05-19 10:16:03 -0300

    Adicionando e atualizando componentes header e balance

--- sprintVP/src/components/header.js
+++ sprintVP/src/components/header.js
@@ -1,35 +1,52 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, Text, StatusBar, TouchableOpacity } from "react-native";
+import { Feather } from '@expo/vector-icons'
 
-const StatusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
+//import LogoVp.png from '../../assets/LogoVp.png'
+
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64
 
 export default function Header({ name }) {
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.initialText}>Selecione uma categoria de pagamento</Text>
-    </View>
-  );
-
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.username}>Bem vindo, {"name"}!</Text>
+                <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+                    <Feather name='user' size={30} color='#fff'/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    zIndex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#460fc9',
-    paddingTop: StatusBarHeight,
-    paddingStart: 16,
-    paddingEnd: 16,
-    paddingBottom: 74,
-  },
-  initialText: {
-    flex: 1,
-    width: '100%',
-    textAlign: 'center',
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    fontfamily: 'sans-serif',
-  }
-});
+    container: {
+        backgroundColor: '#460fc9',
+        paddingTop: statusBarHeight,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingStart: 20,
+        paddingEnd: 20,
+        paddingBottom: 120
+    },
+    content: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    username: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#fff'
+    },
+    buttonUser: {
+        backgroundColor: '#a493e7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50,
+        padding: 8
+    }
+})
+
+// rgb(255, 68, 204)
