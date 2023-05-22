249c4dd0618a178ed7cd6b5ab1b7e264b21e11f3
vitorlvr <vsouza954@gmail.com>
2023-05-22 10:49:37 -0300

    Adicionando switches no menu e melhorias no front

--- sprintVP/src/pages/menu/menu.js
+++ sprintVP/src/pages/menu/menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { NativeBaseProvider, VStack, Switch, Text, List } from 'native-base';
 import { Avatar, ListItem } from '@rneui/themed';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -16,41 +17,83 @@ const MenuPage = () => {
   };
 
   return (
+    <NativeBaseProvider>
       <View style={styles.container}>
-        <LinearGradient
-            colors={['#42007F', '#6600C7']}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            locations={[0.33, 1]}
-            style={styles.botaoUser}
-        >
-          <ListItem containerStyle={{ backgroundColor: 'transparent' }}>
-            <Avatar rounded source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs46fIKd8hYeUZRJ4Q9rFA016EVkt4NhF_lCzkBLFhdQ&s' }} />
-            <ListItem.Content>
-              <ListItem.Title style={{ color: '#fff', fontWeight: 'bold' }}>
-                Ednaldo Pereira
-              </ListItem.Title>
-              <ListItem.Subtitle style={{ color: '#fff' }}>
-                Vice Chairman
-              </ListItem.Subtitle>
-            </ListItem.Content>
-            <ListItem.Chevron color="#fff" />
-          </ListItem>
-        </LinearGradient>
-        
-        <ListItem bottomDivider>
+      <LinearGradient
+        colors={['#42007F', '#6600C7']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        locations={[0.33, 1]}
+        style={styles.botaoUser}
+      >
+        <ListItem containerStyle={{ backgroundColor: 'transparent' }}>
+          <Avatar rounded size="medium" source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs46fIKd8hYeUZRJ4Q9rFA016EVkt4NhF_lCzkBLFhdQ&s' }} />
           <ListItem.Content>
-            <ListItem.Title>Resgate sua senha</ListItem.Title>
+            <ListItem.Title style={{ color: '#fff', fontWeight: 'bold' }}>
+              Ednaldo Pereira
+            </ListItem.Title>
+            <ListItem.Subtitle style={{ color: '#fff' }}>
+              Vice Chairman
+            </ListItem.Subtitle>
           </ListItem.Content>
-          <ListItem.Chevron />
+          <ListItem.Chevron color="#fff" />
         </ListItem>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Title>Entre em contato</ListItem.Title>
-          </ListItem.Content>
-          <ListItem.Chevron />
+      </LinearGradient>
+
+      
+       <ListItem bottomDivider>
+       <ListItem.Content>
+          <ListItem.Title>Modo noturno</ListItem.Title>
+        </ListItem.Content>
+          <Switch
+            style={styles.switchButton}
+            thumbColor="#6600C7"
+            trackColor={{
+              false: '#6600C7',
+              true: '#1ccbcb',
+            }}
+          />
         </ListItem>
+          <ListItem bottomDivider>
+       <ListItem.Content>
+          <ListItem.Title>Autenticação de dois fatores</ListItem.Title>
+        </ListItem.Content>
+          <Switch
+            style={styles.switchButton}
+            thumbColor="#6600C7"
+            trackColor={{
+              false: '#6600C7',
+              true: '#1ccbcb',
+            }}
+          />
+        </ListItem>
+          <ListItem bottomDivider>
+       <ListItem.Content>
+          <ListItem.Title>Notificações</ListItem.Title>
+        </ListItem.Content>
+          <Switch
+            style={styles.switchButton}
+            thumbColor="#6600C7"
+            trackColor={{
+              false: '#6600C7',
+              true: '#1ccbcb',
+            }}
+          />
+        </ListItem>
+      <ListItem bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title>Resgate sua senha</ListItem.Title>
+        </ListItem.Content>
+        <ListItem.Chevron />
+      </ListItem>
+      <ListItem bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title>Entre em contato (se conseguir)</ListItem.Title>
+        </ListItem.Content>
+        <ListItem.Chevron />
+      </ListItem>
       </View>
+    </NativeBaseProvider>
   );
 };
 
@@ -62,7 +105,12 @@ const styles = StyleSheet.create({
   botaoUser: {
     margin: 0,
     paddingTop: 100,
-    paddingBottom: 20,
+    paddingBottom: 5,
+  },
+  switchButton: {
+    margin: 0,
+    marginBottom: 0,
+    alignItems: 'flex-end'
   },
 });
 
