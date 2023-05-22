da7930a1b954367c78f747aba9b1e1933ca328a9
igormaxlima <igormaxnunes@gmail.com>
2023-05-22 17:39:55 -0300

    Implementando novo front-end à função Adicionar Novo Cartão

--- sprintVP/src/pages/meusCartoes/components/ItemClicavel.js
+++ sprintVP/src/pages/meusCartoes/components/ItemClicavel.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Dimensions } from 'react-native';
 
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, AntDesign, Octicons } from '@expo/vector-icons';
 import axios from 'axios';
 
 import ListaCartoes from './ListaCartoes';
+
+const { height } = Dimensions.get('window');
 
 const ItemClicavel = () => {
   const [exibirCampos, setExibirCampos] = useState(false);
@@ -50,48 +52,71 @@ const ItemClicavel = () => {
         </View>
         <MaterialCommunityIcons name="chevron-right" size={24} color="#460fc9" />
       </TouchableOpacity>
-
+      
       {exibirCampos && (
-        <ScrollView>
-          <Text style={styles.textoInput}>Nome do Titular: </Text>
-          <TextInput
-            placeholder="Digite aqui"
-            value={name}
-            onChangeText={setName}
-            returnKeyType="send"
-            style={styles.input}
-            onch
-          />
-          <Text style={styles.textoInput}>Número do Cartão: </Text>
-          <TextInput
-            placeholder="**** **** **** ****"
-            value={number}
-            onChangeText={setNumber}
-            returnKeyType="send"
-            style={styles.input}
-          />
-          <Text style={styles.textoInput}>Data de Validade: </Text>
-          <TextInput
-            placeholder="**/**"
-            value={expirationDate}
-            onChangeText={setExpirationDate}
-            returnKeyType="send"
-            style={styles.input}
-          />
-          <Text style={styles.textoInput}>Código de Segurança - CVV: </Text>
-          <TextInput
-            placeholder="XXX"
-            value={cvv}
-            onChangeText={setCvv}
-            returnKeyType="send"
-            secureTextEntry={true}
-            style={styles.input}
-          />
-          <TouchableOpacity style={styles.botao} onPress={handleAddCard}>
-            <Text style={styles.textoEnviar}>ADICIONAR</Text>
-          </TouchableOpacity>
-          
-        </ScrollView>
+        <View style={styles.container}>
+          <ScrollView >
+            <View style={styles.cartao}>
+
+            </View>
+            
+            <View style={styles.input}>
+              <AntDesign name='user'size={20} color={"#6A1B9A"} style={{marginRight: 10,}}/>
+              <TextInput
+                placeholder="Nome do Titular"
+                value={name}
+                onChangeText={setName}
+                returnKeyType="send"
+                style={styles.textoInput}
+              />
+            </View>
+
+            <View style={styles.input}> 
+            <Octicons name='number'size={20} color={"#6A1B9A"} style={{marginRight: 10,}}/> 
+              <TextInput
+                placeholder="Número do Cartão"
+                value={number}
+                onChangeText={setNumber}
+                returnKeyType="send"
+                style={styles.textoInput}
+              />
+            </View>
+            
+            
+
+            <View style={styles.containerInputsJuntos}>
+              <View style={styles.inputsJuntos}>
+                <AntDesign name='calendar'size={20} color={"#6A1B9A"} style={{marginRight: 10,}}/>
+                <TextInput
+                placeholder="Validade"
+                value={expirationDate}
+                onChangeText={setExpirationDate}
+                returnKeyType="send"
+                style={styles.textoInput}
+                />
+              </View>
+
+              <View style={styles.inputsJuntos}>
+                <MaterialCommunityIcons name='lock'size={20} color={"#6A1B9A"} style={{marginRight: 10,}}/>
+                <TextInput
+                  placeholder="CVV"
+                  value={cvv}
+                  onChangeText={setCvv}
+                  returnKeyType="send"
+                  secureTextEntry={true}
+                  style={[styles.textoInput, {width: 100},]}
+                />
+              </View>
+            </View>  
+
+              
+
+            <TouchableOpacity style={styles.botao} onPress={handleAddCard}>
+              <Text style={styles.textoEnviar}>Cadastrar</Text>
+            </TouchableOpacity>
+            
+          </ScrollView>
+        </View>
       )}
           <ListaCartoes />
         
@@ -100,6 +125,23 @@ const ItemClicavel = () => {
 };
 
 const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      padding: 10,
+      borderRadius: 55,
+      backgroundColor: "#f3f6f9",
+    },
+    cartao: {
+      backgroundColor: "#6A1B9A", 
+      width: 310, 
+      height: 160,
+      borderRadius: 20,
+      alignItems: 'center',
+      marginLeft: 25,
+      marginBottom: 15,
+      
+    },
     button: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -119,27 +161,46 @@ const styles = StyleSheet.create({
         color: "green"
     },
     content: {
-        flexDirection: "column"
+        
     },
     input: {
-        borderWidth: 1,
-        borderColor: "#000",
+        backgroundColor: "#BDBDBD50",
         width: "100%",
-        borderRadius: 5,
-        padding: 8,
+        height: 60,
+        marginTop: 14,
+        borderRadius: 8,
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    containerInputsJuntos: {
+      flexDirection: 'row',
+    },
+    inputsJuntos: {
+      backgroundColor: "#BDBDBD50",
+      width: "45%",
+      height: 60,
+      marginTop: 14,
+      borderRadius: 8,
+      marginHorizontal: 10,
+      padding: 12,
+      flexDirection: 'row',
+      alignItems: 'center',
     },
     textoInput: {
         fontSize: 15,
-        fontFamily: "MontserratRegular",
+        fontFamily: "MontserratBold",
         padding: 8,
         color: "#000",
     },
     botao: {
       alignItems: "center",
-      width: 350, 
+      justifyContent: 'center',
+      width: "100%", 
+      height: 55,
       marginVertical: 20,
-      borderRadius: 5,
-      backgroundColor: "#5619b4",
+      borderRadius: 55,
+      backgroundColor: "#6A1B9A",
     },
     textoEnviar: {
         fontSize: 20,
